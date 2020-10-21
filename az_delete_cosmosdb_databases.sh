ebcb97c5e32b306b4c5f2a51a227783073908d74
Mohammad Derakhshani <moderakh@users.noreply.github.com>
2020-10-21 12:23:08 -0700

    cleanup

--- az_delete_cosmosdb_databases.sh
+++ az_delete_cosmosdb_databases.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 # author: Mohammad Derakhshani <moderakh>
-# version: 0.9
+# version: 0.9.1
 # NOTE: this will delete all the databases and containers in your cosmos db account
 # if you don't provide any option it will target Cosmos Emualtor running on Parallels on Mac accessable on 10.37.129.3:8081
-# usage when running against emulator:
-#   bash az_delete_cosmosdb_databases.sh
+# usage when running against emulator reachable on https://10.37.129.3:8081:
+#   bash az_delete_cosmosdb_databases.sh -e https://10.37.129.3:8081
 # usage when running against prod acocunt
 #   bash az_delete_cosmosdb_databases.sh -g YOUR_RESOURCE_GROUP -n YOUR_COSMOSDB_ACCOUNT_NAME -s YOUR_SUB_NAME
 set -e
 
+EMULATOR_HOST="https://10.37.129.3:8081"
+
 if [ -z "$1" ]; then
-  export CURL_CA_BUNDLE=""
-  opts="--url-connection https://10.37.129.3:8081 --key C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
+  emulatorHost=$EMULATOR_HOST
 else
 
   while (( "$#" )); do
@@ -28,6 +29,12 @@ else
         subscription="$2"
         shift 2
         ;;
+      -e|--emulatorHost)
+        emulatorHost="$2"
+        #TODO validate that no other arg is set
+        shift 2
+        ;;
+
       -*|--*=) # unsupported flags
         echo "Error: Unsupported flag $1" >&2
         exit 1
@@ -38,8 +45,13 @@ else
         ;;
     esac
   done
+fi
 
-  public_ip=`curl ifconfig.me` > /dev/null
+if [ -n "$emulatorHost" ]; then 
+  export CURL_CA_BUNDLE=""
+  opts="--url-connection $EMULATOR_HOST --key C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
+else 
+  #public_ip=`curl ifconfig.me` > /dev/null
   opts="-n $accountName -g $resource_group"
   if [ -n "$subscription" ]; then
     az account set --subscription "$subscription"
@@ -54,7 +66,6 @@ if [ "$db_cnt" -eq 0 ]; then
   echo "Finished!"
   exit 0
 fi
-
  
 echo "Deleting $db_cnt databases..."
 az cosmosdb database list $opts -o table 2>/dev/null | tail -n +3 | cut -d' ' -f1 | xargs -I '{}' az cosmosdb database delete $opts -d '{}' --yes 2>/dev/null 
